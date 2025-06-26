85547c5fc070003eb0818013402c0b8fb3477e97
Shilam Sai Bhargavi <178761237+ssaibhargavi@users.noreply.github.com>
2025-06-26 20:23:09 +0530

    Update no_arg_no_return.c

--- no_arg_no_return.c
+++ no_arg_no_return.c
@@ -10,16 +10,3 @@ int main()
     Hello();
     return 0;
 }
-#include<stdio.h>
-void number()
-{
-    int num;
-    printf("Enter a number:");
-    scanf("%d",&num);
-    printf("You have entered %d",num);
-}
-int main()
-{
-    number();
-    return 0;
-}
