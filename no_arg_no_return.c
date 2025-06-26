6a005cfb38132e248d69f9ba61cca2cbfb583145
Shilam Sai Bhargavi <178761237+ssaibhargavi@users.noreply.github.com>
2025-06-26 20:14:13 +0530

    Create no_arg_no_return.c

---
+++ no_arg_no_return.c
@@ -0,0 +1,25 @@
+// Function with no arguments and no return value.
+
+#include<stdio.h>
+void Hello()
+{
+    printf("Hello all!!!");
+}
+int main()
+{
+    Hello();
+    return 0;
+}
+#include<stdio.h>
+void number()
+{
+    int num;
+    printf("Enter a number:");
+    scanf("%d",&num);
+    printf("You have entered %d",num);
+}
+int main()
+{
+    number();
+    return 0;
+}
