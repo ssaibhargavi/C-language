4fd71ac90dae1fe7bffbe50c9df00c7239b33a36
Shilam Sai Bhargavi <178761237+ssaibhargavi@users.noreply.github.com>
2025-07-24 18:14:23 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,14 @@
+// Calculating sum 
+
+#include <stdio.h>
+int main() {
+    int num1;
+    float num2;
+    scanf("%d",&num1);
+    printf("num1 = %d\n",num1);
+    scanf("%f",&num2);
+    printf("num2 = %.2f\n",num2);
+    float sum = num1+num2;
+    printf("sum = %.2f\n",sum);
+    return 0;
+}
